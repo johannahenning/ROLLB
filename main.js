a9e45ad74d3d6cd81bc606b2cf0f40ab76a8cce8
manufohrer <manufohrer@gmx.de>
2017-11-07 14:14:15 +0100

    disco

--- main.js
+++ main.js
@@ -117,28 +117,24 @@
 		audio.play();
 		setTimeout(function() {
 			setColor(255, 0, 0);
-			roll(Math.round(180), 60, 1);
 		}, 0);
 		setTimeout(function() {
 			roll(Math.round(180), 60, 1);
 		}, 500);
 		setTimeout(function() {
 			setColor(0, 255, 0);
-			roll(Math.round(0), 60, 1);
 		}, 1000);
 		setTimeout(function() {
 			roll(Math.round(0), 60, 1);
 		}, 1500);
 		setTimeout(function() {
 			setColor(0, 0, 255);
-			roll(Math.round(180), 60, 1);
 		}, 2000);
 		setTimeout(function() {
 			roll(Math.round(180), 60, 1);
 		}, 2500);
 		setTimeout(function() {
 			setColor(0, 0, 0);
-			roll(Math.round(0), 60, 1);
 		}, 3000);
 		setTimeout(function() {
 			roll(Math.round(0), 60, 1);
