56594d74cf59ec64a2c256b91398d092181725a7
johannahenning <hennjoha@gmx.de>
2017-12-21 16:03:52 +0100

    neuer Code

---
+++ bb8.js
@@ -0,0 +1,73 @@
+"use strict"
+
+var Cylon = require('cylon');
+var keypress = require("keypress");
+
+
+    Cylon.robot({
+      connections: {
+        bluetooth: { adaptor: 'central', uuid: '4e51973b007a40db9e45a207c39a8065', module: 'cylon-ble'}
+      },
+
+      devices: {
+        bb8: { driver: 'bb8', module: 'cylon-sphero-ble'}
+      },
+
+      work: function(my) {
+
+        function handle(ch, key) {
+
+          if (key.ctrl && key.name === "c") {
+              process.stdin.pause();
+              process.exit();
+          }
+
+          switch (key.name) {
+
+            case "c":
+            var myColor = setInterval(function () {
+              my.bb8.randomColor();
+            }, 1000);
+              // my.bb8.randomColor();
+            break;
+
+            case "k":
+              my.bb8.randomColor();
+            break;
+
+            case "w":
+              my.bb8.randomColor();
+              my.bb8.roll(100, 90);
+            break;
+
+            case "d":
+              my.bb8.randomColor();
+              my.bb8.roll(100, 180);
+            break;
+
+            case "s":
+              my.bb8.randomColor();
+              my.bb8.roll(100, 270);
+            break;
+
+            case "a":
+              my.bb8.randomColor();
+              my.bb8.roll(100, 0);
+            break;
+
+            case "space":
+              my.bb8.stop();
+            break;
+            }
+            //
+            // my.bb8.detectCollisions();
+            // my.bb8.stopOnDisconnect();
+          }
+
+          keypress(process.stdin);
+          process.stdin.on("keypress", handle);
+
+          process.stdin.setRawMode(true);
+          process.stdin.resume();
+      }
+    }).start();
