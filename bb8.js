487e82630e000dee828f42f1434810dc326a9014
oberlixgmbh <simon.prell@googlemail.com>
2017-12-21 16:30:30 +0100

    added mqtt

--- bb8.js
+++ bb8.js
@@ -2,7 +2,17 @@
 
 var Cylon = require('cylon');
 var keypress = require("keypress");
+var mqtt = require ("mqtt");
+client.on('connect', function () {
+    client.subscribe('presence')
+    client.publish('presence', 'Hello mqtt')
+})
 
+client.on('message', function (topic, message) {
+    // message is Buffer
+    console.log(message.toString())
+    client.end()
+})
 
     Cylon.robot({
       connections: {
